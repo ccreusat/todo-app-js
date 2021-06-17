d681bbc9a02136cbfef7a11bfa11aceb468161fb
Clément Creusat <clement.creusat@gmail.com>
2021-06-17 11:05:30 +0200

    push

---
+++ theme-switch.js
@@ -0,0 +1,42 @@
+// THEME SWITCHER //
+const switchToggle = document.getElementById("switch");
+
+// Display the default theme using user's OS configuration (Dark Mode on / off) and using @media color-scheme
+// If theme is already stored in local storage, use it. Otherwise, get it from the OS.
+(function () {
+	"use strict";
+
+	let getCurrentTheme = useLocalStorage.getItem("theme") || "light";
+
+	if (getCurrentTheme) {
+		document.body.className = getCurrentTheme;
+	}
+
+	window
+		.matchMedia("(prefers-color-scheme: dark)")
+		.addEventListener("change", event => {
+			if (event.matches) {
+				document.body.className = "dark";
+			} else {
+				document.body.className = "light";
+			}
+		});
+})();
+
+// Toggle theme using Moon & Sun Icon
+const switchTheme = currentTheme => {
+	let newTheme;
+	let bodyClassName = document.body.className;
+
+	// Looking which theme is actually used
+	if (bodyClassName === currentTheme) {
+		newTheme = "dark";
+	} else {
+		newTheme = "light";
+	}
+
+	document.body.className = newTheme;
+	useLocalStorage.setItem("theme", newTheme);
+};
+
+switchToggle.onclick = () => switchTheme("light");
---
+++ use-local-storage.js
@@ -0,0 +1,20 @@
+const useLocalStorage = {
+	// get item from local storage
+	getItem: key => {
+		let getParsedItem = JSON.parse(window.localStorage.getItem(key));
+		// if @key exists return it, otherwise, return a string.
+		if (getParsedItem) {
+			return getParsedItem;
+		} else {
+			return "";
+		}
+	},
+	// set item from local storage. pass a key and element to be stored
+	setItem: (key, element) => {
+		window.localStorage.setItem(key, JSON.stringify(element));
+	},
+	// remove item from local storage using the @key
+	removeItem: key => {
+		window.localStorage.removeItem(key);
+	},
+};
